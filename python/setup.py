337f8f43ec33efa043f88d549fc217eb13ce9f5f
Vanova <iva427@mail.ru>
2019-07-25 11:58:16 +0800

    add LD_LIBRARY_PATH to run pyrirgen

--- python/setup.py
+++ python/setup.py
@@ -7,23 +7,23 @@ import numpy as np
 # python3 setup.py build_ext --inplace
 
 extensions = [
-	Extension("*", ["pyrirgen.pyx"],
-		include_dirs=[
-			os.path.abspath("."),
-		],
-		library_dirs=[os.path.abspath(".")],
-		libraries=["rirgen"],
-		language='c++'
-	),
+    Extension("*", ["pyrirgen.pyx"],
+              include_dirs=[
+                  os.path.abspath("."),
+              ],
+              library_dirs=[os.path.abspath(".")],
+              libraries=["rirgen"],
+              language='c++'
+              ),
 ]
 
 setup(
-	name = 'pyrirgen',
-	ata_files = [('', ['librirgen.so'])],
-	ext_modules=cythonize(extensions, compiler_directives = {
-		'language_level': 3, # Python 3
-		'embedsignature': True, # add method signature to docstrings, thus tools can display it after compilation
-		'c_string_encoding': 'default',
-		'c_string_type': 'str',
-	}),
+    name='pyrirgen',
+    ata_files=[('', ['librirgen.so'])],
+    ext_modules=cythonize(extensions, compiler_directives={
+        'language_level': 3,  # Python 3
+        'embedsignature': True,  # add method signature to docstrings, thus tools can display it after compilation
+        'c_string_encoding': 'default',
+        'c_string_type': 'str',
+    }),
 )
